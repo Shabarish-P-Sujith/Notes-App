af4db24e6767c74a4744ace8d26dc9da6fe10a25
unknown <SBUP@SCS-LB3-LPT27.sbup.local>
2025-01-29 14:17:27 +0530

    Stated About Page - 01

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -2,7 +2,16 @@ exports.homepage = function(req, res) {
     res.render('index', { 
         locals1: { // always write locals inside render
             title: "Note App",
-            description: "Creates using NodeJS"
+            description: "Created using NodeJS"
+        }
+    });
+}
+
+exports.about = function(req, res) {
+    res.render('about', { 
+        locals1: { // always write locals inside render
+            title: "About - Note App",
+            description: "Created using NodeJS"
         }
     });
 }

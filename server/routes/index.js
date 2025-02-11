5ff11d7d16e4b924c63128954a58974250b5ec40
Shabarish-P-Sujith <shabarishpsujith@gmail.com>
2025-02-12 00:51:18 +0530

    defining Error route

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,6 @@ const mainController = require('../controllers/mainController');
 
 router.get('/', mainController.homepage);
 router.get('/about', mainController.about);
+router.get('*', mainController.error);
 
 module.exports = router;
